ebcd0d49e803bf8ca2fc4be408da2629b5f25eb6
Julien Flawinne <cptflaw@outlook.com>
2018-01-28 11:43:11 +0100

    add requirements file

--- domain/observation/observation.py
+++ domain/observation/observation.py
@@ -1,5 +1,5 @@
 class Observation:
-    def __init__(self, id=None, start_date=None waterfall=None, payload=None):
+    def __init__(self, id=None, start_date=None, waterfall=None, payload=None):
         self.id = id
         self.start_date = start_date
         self.waterfall = waterfall
