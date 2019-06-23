04204fd2be6eb18316dff31f11016b90b6a2de8d
Red Boumghar <redouane.boumghar@free.fr>
2019-06-23 21:12:08 +0000

    config: hardcoded default configuration
    When the configuration file is not found, the default configuration is used.

--- glouton/shared/config.py
+++ glouton/shared/config.py
@@ -3,10 +3,28 @@ import json
 
 def read():
     config = None
+    default_config = {
+                "DEFAULT": {
+                    "API_URL": "https://network.satnogs.org/api/",
+                    "HTTPS_PROXY": "",
+                    "HTTP_PROXY": ""
+                },
+                "MODULES": {
+                    "PAYLOAD": [],
+                    "WATERFALL": [],
+                    "DEMODDATA": [],
+                    "FORALL": []
+                },
+                "LOGFILE": "glouton.log"
+            }
     try:
-        with open('glouton-config.json', 'r') as f:
-        #with open(os.path.join(os.path.dirname(__file__), '../config.json'), 'r') as f:
-            config = json.load(f)
-    except FileNotFoundError as enfe_err:
-        print("error: glouton configuration's file hasn't been found. Copy it from the sources and name it: ./glouton-config.json")
+        if os.path.exists("glouton-config.json"):
+            with open('glouton-config.json', 'r') as f:
+                config = json.load(f)
+        else:
+            config = default_config
+
+    except Exception as eee:
+        print("error: ", eee)
+        config = default_config
     return config
