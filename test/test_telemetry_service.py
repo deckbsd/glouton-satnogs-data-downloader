e6fd66e24684cef0cc4da20c002038a9bc59bbce
deckbsd <deckbsd@protonmail.com>
2019-08-12 14:08:47 +0200

    add unit test on telemetry service

---
+++ test/test_telemetry_service.py
@@ -0,0 +1,28 @@
+from glouton.services.telemetry.telemetryService import TelemetryService
+from glouton.repositories.frame.frameRepo import FrameRepo
+from glouton.domain.parameters.programCmd import ProgramCmd
+
+def test_loading_all_repository_types():
+    params = ProgramCmd(None, None, None, None, None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None)
+
+    service = TelemetryService(params)
+    repos = service.filter_repositories()
+    assert len(repos) == 1
+    assert isinstance(repos[0], FrameRepo)
