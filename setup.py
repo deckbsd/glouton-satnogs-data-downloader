e3b04f5d6975c31f0d12ef3afb7d5c95bf93ed51
deckbsd <deckbsd@protonmail.com>
2019-08-12 14:42:38 +0200

    release 0.3.0 to 0.4.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    version="0.3.0",
+    version="0.4.0",
     name="glouton",
     description="`Glouton is a cli program which helps you downloading satnogs data`",
     url="https://github.com/deckbsd/glouton-satnogs-data-downloader.git",
