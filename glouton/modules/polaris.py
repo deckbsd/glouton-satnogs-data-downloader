7f935d92f274ff7aacf97f84712e63a2f20db9a9
deckbsd <deckbsd@protonmail.com>
2019-08-13 13:49:53 +0200

    update the polaris module for better json object

--- glouton/modules/polaris.py
+++ glouton/modules/polaris.py
@@ -1,6 +1,8 @@
 
 from glouton.modules.telemetryModuleBase import TelemetryModuleBase
 import json
+import os
+
 
 class Polaris(TelemetryModuleBase):
     def __init__(self, wdir):
@@ -11,15 +13,26 @@ class Polaris(TelemetryModuleBase):
         timestamp = telemetry['timestamp']
         if not frame:
             print('no frame for ' + timestamp)
-        
+
         json_file = full_path + '/' + timestamp.replace(':', '-') + '.json'
-        telemetry_obj = {
-            "timestamp": timestamp,
-            "frame": frame
-        }
+        if os.path.exists(json_file):
+            with open(json_file) as json_file_read:
+                telemetry = json.load(json_file_read)
+                telemetry['telemetry'].append({
+                    "timestamp": timestamp,
+                    "frame": frame
+                })
+            telemetry_obj = telemetry
+        else:
+            telemetry_obj = {"telemetry": [{
+                "timestamp": timestamp,
+                "frame": frame
+            }]}
+
         json_telemetry = json.dumps(telemetry_obj)
-        with open(json_file, 'a') as f:
+        with open(json_file, 'w') as f:
             f.write(json_telemetry)
 
         self.count += 1
-        print('Timestamp ' + timestamp + ' Frame ' +  frame + ' count ' + str(self.count))
+        print('Timestamp ' + timestamp + ' Frame ' +
+              frame + ' count ' + str(self.count))
