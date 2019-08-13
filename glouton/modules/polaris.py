6b393db5b59a8ab6c04cd9301f0c962ad1b46ca4
deckbsd <deckbsd@protonmail.com>
2019-08-13 11:11:51 -0700

    add exception catching on polaris module

--- glouton/modules/polaris.py
+++ glouton/modules/polaris.py
@@ -1,5 +1,6 @@
 
 from glouton.modules.telemetryModuleBase import TelemetryModuleBase
+from glouton.shared.logger import logger
 import json
 import os
 
@@ -10,29 +11,32 @@ class Polaris(TelemetryModuleBase):
         self.count = 0
 
     def runAfterDownload(self, frame, full_path, telemetry):
-        timestamp = telemetry['timestamp']
-        if not frame:
-            print('no frame for ' + timestamp)
+        try:
+            timestamp = telemetry['timestamp']
+            if not frame:
+                print('no frame for ' + timestamp)
 
-        json_file = full_path + '/' + timestamp.replace(':', '-') + '.json'
-        if os.path.exists(json_file):
-            with open(json_file) as json_file_read:
-                telemetry = json.load(json_file_read)
-                telemetry['telemetry'].append({
+            json_file = full_path + '/' + timestamp.replace(':', '-') + '.json'
+            if os.path.exists(json_file):
+                with open(json_file) as json_file_read:
+                    telemetry = json.load(json_file_read)
+                    telemetry['telemetry'].append({
+                        "timestamp": timestamp,
+                        "frame": frame
+                    })
+                telemetry_obj = telemetry
+            else:
+                telemetry_obj = {"telemetry": [{
                     "timestamp": timestamp,
                     "frame": frame
-                })
-            telemetry_obj = telemetry
-        else:
-            telemetry_obj = {"telemetry": [{
-                "timestamp": timestamp,
-                "frame": frame
-            }]}
+                }]}
 
-        json_telemetry = json.dumps(telemetry_obj)
-        with open(json_file, 'w') as f:
-            f.write(json_telemetry)
+            json_telemetry = json.dumps(telemetry_obj)
+            with open(json_file, 'w') as f:
+                f.write(json_telemetry)
 
-        self.count += 1
-        print('Timestamp ' + timestamp + ' Frame ' +
-              frame + ' count ' + str(self.count))
+            self.count += 1
+            print('Timestamp ' + timestamp + ' Frame ' +
+                frame + ' count ' + str(self.count))
+        except Exception as ex:
+            logger.Error(ex)
